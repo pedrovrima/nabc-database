90e931652f35581b4f360e5017ead49ebed77577
Pedro Martins <pedrovrima@gmail.com>
2021-10-18 13:43:55 -0300

    banders and session working

---
+++ pages/api/session_id.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function getSessions(req, res) {
+  const id =   (JSON.parse(req.body)).id;
+  console.log(   (JSON.parse(req.body)).id)
+  const session = await prisma.session.findFirst({
+    where: { id },
+    include: {
+      chair: true,
+      evaluations: {include:{bander:true,evaluators:true}},
+     }
+  });
+
+  res.json(session);
+}
